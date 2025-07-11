ac6340bc19b90090cbead8b138c9a3c31c58f7b6
EddyGiusepe <eddychirinos.unac@gmail.com>
2025-07-11 00:56:43 -0300

    Meu schema para uma saída estruturada

---
+++ shared/schemas.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+"""
+Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+
+Script schema.py
+================
+Definimos um esquema TravelRequest usando o Pydantic. Isso 
+garante que todos os agentes concordem com a estrutura das 
+solicitações recebidas, o que inclui o destino, as datas da 
+viagem e o orçamento do usuário.
+
+Esta class ajuda em:
+
+* Manter a entrada consistente para todos os agentes.
+* Adicionando validação automática com `FastAPI`.
+* Simplificando a reutilização de código.
+"""
+from pydantic import BaseModel
+
+class TravelRequest(BaseModel):
+    destination: str
+    start_date: str
+    end_date: str
+    budget: float
+    
