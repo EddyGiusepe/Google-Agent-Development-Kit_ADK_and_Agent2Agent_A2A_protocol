96a60438c79db2a379b794ee51d696725c0f5e84
EddyGiusepe <eddychirinos.unac@gmail.com>
2025-07-13 20:25:35 -0300

    O gerenciador de tarefa para o agente de atividades

--- agents/activities_agent/task_manager.py
+++ agents/activities_agent/task_manager.py
@@ -1,4 +1,9 @@
+#! /usr/bin/env python3
+"""
+Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+"""
 from .agent import execute
+
 
 async def run(payload):
     return await execute(payload)
