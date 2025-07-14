df46d07de67eae56fd44ae408acdd81163c27b2e
EddyGiusepe <eddychirinos.unac@gmail.com>
2025-07-14 17:26:36 -0300

    Meu CLIENT usando a2a

--- common/a2a_client.py
+++ common/a2a_client.py
@@ -4,18 +4,19 @@ Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
 
 Script a2a_client.py
 ====================
-Este utilitário assíncrono leve permite que qualquer 
-agente (especialmente o host) invoque outro agente 
+Este utilitário assíncrono leve permite que qualquer
+agente (especialmente o host) invoque outro agente
 usando o protocolo A2A chamando o ponto de extremidade `/run`.
 
-Este utilitário envia de forma assíncrona uma solicitação POST para o 
-endpoint /run de outro agente usando httpx. Ele retorna a resposta 
+Este utilitário envia de forma assíncrona uma solicitação POST para o
+endpoint /run de outro agente usando httpx. Ele retorna a resposta
 JSON analisada e gera um erro se a solicitação falhar.
 
-Usaremos esse utilitário em nosso agente host para chamar 
+Usaremos esse utilitário em nosso agente host para chamar
 `flight_agent`, `stay_agent`, e `activities_agent`.
 """
 import httpx
+
 
 async def call_agent(url, payload):
     async with httpx.AsyncClient() as client:
